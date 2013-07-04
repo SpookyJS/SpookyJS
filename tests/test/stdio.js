552de796c9822d68202147484ac5ff1acba83ec9
kpdecker <kpdecker@gmail.com>
2013-07-04 01:09:21 -0500

    Allow casper restart in stdio server
    Always run the server event loop while casper is not running. This hacks
    around the blocking behavior of the phantom readLine implementation.

--- tests/test/stdio.js
+++ tests/test/stdio.js
@@ -69,4 +69,80 @@ describe('Spooky provides a stdio transport', function () {
             });
         });
     });
+
+    it('allows restarts after completion', function (done) {
+        var out = [];
+
+        function onConsole(line) {
+            if (line === 'Fourth') {
+                expect(out).to.contain('First');
+                expect(out).to.contain('Second');
+                expect(out).to.contain('Third');
+                context.spooky.removeListener('console', onConsole);
+                done();
+                return;
+            }
+
+            out.push(line);
+        }
+        context.spooky.on('console', onConsole);
+
+        context.spooky.start();
+        context.spooky.then(function () {
+            this.echo('First');
+        });
+        context.spooky.then(function () {
+            this.echo('Second');
+        });
+        context.spooky.run(function() {
+            this.emit('test.done');
+        });
+
+        context.spooky.on('test.done', function() {
+            context.spooky.start();
+            context.spooky.then(function () {
+                this.echo('Third');
+            });
+            context.spooky.then(function () {
+                this.echo('Fourth');
+            });
+            context.spooky.run();
+        });
+    });
+
+    it('allows restarts queued before completion', function (done) {
+        var out = [];
+
+        function onConsole(line) {
+            if (line === 'Fourth') {
+                expect(out).to.contain('First');
+                expect(out).to.contain('Second');
+                expect(out).to.contain('Third');
+                context.spooky.removeListener('console', onConsole);
+                done();
+                return;
+            }
+
+            out.push(line);
+        }
+        context.spooky.on('console', onConsole);
+
+        context.spooky.start();
+        context.spooky.then(function () {
+            this.echo('First');
+        });
+        context.spooky.then(function () {
+            this.echo('Second');
+        });
+        context.spooky.run(function() {});
+
+        context.spooky.start();
+        context.spooky.then(function () {
+            this.echo('Third');
+        });
+        context.spooky.then(function () {
+            this.echo('Fourth');
+        });
+        context.spooky.run();
+    });
 });
