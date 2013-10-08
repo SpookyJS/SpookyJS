3872c81316487c212241b5587cd604f629c4aca4
Lon Ingram <lawnsea@gmail.com>
2013-10-07 22:42:05 -0500

    Set up grunt and add jshint task
    close #76

---
+++ Gruntfile.js
@@ -0,0 +1,58 @@
+// Gruntfile.js
+module.exports = function (grunt) {
+  // load all grunt tasks matching the `grunt-*` pattern
+  require('load-grunt-tasks')(grunt);
+
+  grunt.initConfig({
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        unused: true,
+        boss: true,
+        eqnull: true,
+        globals: {}
+      },
+      gruntfile: {
+        src: 'Gruntfile.js',
+        options: {
+          node: true
+        }
+      },
+      node_libs: {
+        src: ['lib/spooky.js', 'lib/spooky/**/*.js'],
+        options: {
+          node: true
+        }
+      },
+      phantom_libs: {
+        src: ['lib/bootstrap.js', 'lib/bootstrap/**/*.js'],
+        options: {
+          phantom: true
+        }
+      },
+      tests: {
+        src: ['tests/test/**/*.js'],
+        options: {
+          phantom: true,
+          node: true,
+          browser: true,
+          globals: {
+            it: true,
+            describe: true,
+            beforeEach: true,
+            afterEach: true
+          }
+        }
+      }
+    }
+  });
+
+  grunt.registerTask('default', []);
+};
